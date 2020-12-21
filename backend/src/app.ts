894bae86738f7f6465d3240fdee410a21d5f95e3
Griha3212 <Griha3212@yandex.ru>
2020-12-21 18:07:02 +0300

    update, but still listen socket problem exists

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,22 +1,32 @@
+/* eslint-disable no-console */
 import chalk from 'chalk';
 import express, { Request, Response } from 'express';
 
+import * as io from 'socket.io';
 
 const port: Number = 3005;
 console.log(chalk.yellow('process.env.PORT', process.env.PORT));
 
+declare global {
+  namespace NodeJS {
+    interface Global {
+      SocketServer: io.Server
+    }
+  }
+}
+
 // Create a new express application instance
 export const app: express.Application = express();
 
-
 const server = app.listen(port, () => {
-    console.log(chalk.yellow(`Example app listening on port ${port}!`));
+  console.log(chalk.yellow(`Example app listening on port ${port}!`));
 });
 
-
-
-
+// sockets
+global.SocketServer = io.listen(server, { pingInterval: 5000, pingTimeout: 10000, cookie: false });
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
+
+export default app;
