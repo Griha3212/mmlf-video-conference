0168dfc55fd3fe389f9fef4a5cec3ae5e4ec8fea
Griha3212 <Griha3212@yandex.ru>
2021-01-21 19:28:25 +0300

    delete unused code

--- backend/src/controllers/stats.controller.ts
+++ backend/src/controllers/stats.controller.ts
@@ -2,19 +2,13 @@
 /* eslint-disable import/prefer-default-export */
 import { Request, Response, NextFunction } from 'express';
 import { getRepository } from 'typeorm';
-import Channels from '../entities/channels';
-import Sessions from '../entities/sessions';
 import Speakers from '../entities/speakers';
 import Users from '../entities/users';
-import Votes from '../entities/votes';
 import allErrors from '../utils/errors';
-import { io } from '../server';
 
 // TO DO
 export const getStats = async (req: Request, res: Response, next: NextFunction) => {
   const usersRepository = await getRepository(Users);
-  const sessionsRepository = await getRepository(Sessions);
-  const channelsRepository = await getRepository(Channels);
   const speakersRepository = await getRepository(Speakers);
 
   try {
@@ -31,9 +25,9 @@ export const getStats = async (req: Request, res: Response, next: NextFunction) 
 
     if (!foundUser.hasAccessToStatisticPage) throw new Error(allErrors.notStatsAccount);
 
-    const foundSpeakers = await speakersRepository.find({ relations: ['usersWhoWatchedSpeaker', 'votes', 'votes.user'] });
-
-    console.log('foundSpeakers :>> ', foundSpeakers);
+    const foundSpeakers = await speakersRepository.find(
+      { relations: ['usersWhoWatchedSpeaker', 'votes', 'votes.user'] },
+    );
 
     res.status(200).send(foundSpeakers);
   } catch (error) {
