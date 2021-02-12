f8279c3f2b7a776633d428127b933901a4358ce2
Griha3212 <Griha3212@yandex.ru>
2021-02-12 15:21:48 +0300

    change test video

--- backend/src/utils/seeds/seedChannels.ts
+++ backend/src/utils/seeds/seedChannels.ts
@@ -9,7 +9,7 @@ export const seedMockedChannels = async () => {
   // test links now
   const channelsToSeed = [{
     number: 1,
-    link: 'https://facecast.net/v/wi346l',
+    link: 'https://www.youtube.com/embed/wnhvanMdx4s',
     startChannelSessionLetter: 'Пленарная сессия',
     startChannelSessionDescription: 'Развитие логистики и управления цепями поставок в России и мире',
   },
