3acbf5ee9511759d04ef503140795dc3d02b5b6f
Griha3212 <Griha3212@yandex.ru>
2021-02-13 11:32:33 +0300

    update to 120 sec timing

--- frontend/src/containers/User/UserPage.tsx
+++ frontend/src/containers/User/UserPage.tsx
@@ -187,7 +187,7 @@ const UserPage: FC = () => {
           localStorage.setItem(`${String(activeSpeakerInfo && activeSpeakerInfo.id)}`, '0');
         } else {
           if (initialValueCurrentSpeaker !== 'viewed') {
-            if (initialValueCurrentSpeaker === '5') {
+            if (initialValueCurrentSpeaker === '120') {
               localStorage.setItem(`${String(activeSpeakerInfo && activeSpeakerInfo.id)}`, 'viewed');
               // send to back viewed status
               updateWatchedSpeakersSingleSpeaker(activeSpeakerInfo.id);
@@ -208,7 +208,7 @@ const UserPage: FC = () => {
           localStorage.setItem(`${String(dataForUser.channelUserInfo.activeSession.name)}`, '0');
         } else {
           if (initialValueCurrentSession !== 'viewed') {
-            if (initialValueCurrentSession === '5') {
+            if (initialValueCurrentSession === '120') {
               localStorage.setItem(`${String(dataForUser.channelUserInfo.activeSession.name)}`, 'viewed');
               // send to back viewed status
               updateWatchedSpeakersAllSpeakersInSession(
